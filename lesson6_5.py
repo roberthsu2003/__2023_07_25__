2f6974c06c1a6f643cbfa90447266645520c19f6
徐國堂 <roberthsu2003@gmail.com>
2023-08-10 13:59:50 +0000

    modify

--- lesson6_5.py
+++ lesson6_5.py
@@ -29,7 +29,7 @@ def play_game():
             print("超出範圍")
 
 while True:
-    play_game()    
+    play_game()#呼叫function    
     play_again = input("您還要繼續嗎?(y or n):")
     if play_again == 'n':        
         break
